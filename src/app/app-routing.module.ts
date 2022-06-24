d2ffedc4ef89e9b9fe5330f61aafa4c419eb0abc
KNT <knt@gmail.com>
2022-06-24 16:05:39 +0700

    new project

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BodyComponent } from './components/body/body.component';
+import { NewsDetailComponent } from './components/news-detail/news-detail.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: BodyComponent},
+  {path: 'post', component: NewsDetailComponent},
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { BodyComponent } from './components/body/body.component';
+import { NewsDetailComponent } from './components/news-detail/news-detail.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    BodyComponent,
+    NewsDetailComponent
   ],
   imports: [
     BrowserModule,
